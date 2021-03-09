167fb29467020873925eb1aabd840596e161f319
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-09 10:02:03 -0700

    Add basic post request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
     .then(response => response.json())
 }
+
+export const postUrls = data => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+}
